cc4460898acbc806c615e77ed72b8dd003a9c434
Tzvetelin Tzonchev <cconchev@mail.bg>
2022-11-01 19:27:15 +0200

    Service collection extensions methods

---
+++ MobileShop/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using Microsoft.EntityFrameworkCore;
+    using MobileShop.Infrasructure.Data;
+
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        { 
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
